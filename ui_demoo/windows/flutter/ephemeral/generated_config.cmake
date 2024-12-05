0b487c308565e192478598bd4f500373552924da
aswinghosh <aswinghosh4@gmail.com>
2024-12-05 12:10:41 +0530

    Product_app
    Product application build by using flutter .

---
+++ ui_demoo/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\aswin\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\aswin\\OneDrive\\ドキュメント\\ui_demoo" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\aswin\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\aswin\\OneDrive\\ドキュメント\\ui_demoo"
+  "FLUTTER_ROOT=C:\\Users\\aswin\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\aswin\\OneDrive\\ドキュメント\\ui_demoo\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\aswin\\OneDrive\\ドキュメント\\ui_demoo"
+  "FLUTTER_TARGET=C:\\Users\\aswin\\OneDrive\\ドキュメント\\ui_demoo\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\aswin\\OneDrive\\ドキュメント\\ui_demoo\\.dart_tool\\package_config.json"
+)
